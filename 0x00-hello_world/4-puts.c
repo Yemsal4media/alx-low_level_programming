746ff53c3c6369594212a44a59d78b3ab7c60901
yemsal4media <yemsalforlove2012@gmail.com>
2023-03-20 04:39:47 -0700

    betty style documentation correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * A c proram
+ * to prints with (put) function
+ * to also Return 0
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
