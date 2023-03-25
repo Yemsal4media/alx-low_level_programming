bdd29c18d51bf1f41c45bf3b4b87cf139f1c341c
yemsal4media <yemsalforlove2012@gmail.com>
2023-03-24 21:55:34 -0700

    i dont understand this task 0

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,6 +3,9 @@
 
 #include <stdio.h>
 
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 void positive_or_negative(int i);
 
 #endif /* MAIN_H */
