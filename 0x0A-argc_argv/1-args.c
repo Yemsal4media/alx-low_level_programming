a19387bb77a22060d82c29a97195c383f8756728
yemsal4media <yemsalforlove2012@gmail.com>
2023-04-10 09:21:16 -0700

    argc_argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib>
+
+/**
+ * main -  program that print a number of argument, 
+ *           followed by a new line
+ * @argc: holds number of arguments passed
+ * @argv: an array pointer to hold the argument passed
+ *
+ * Return: 0
+*/
+
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+    int i = 0, m;
+
+    while (i < argv)
+    {
+        m = i;
+        i++
+    }
+    printf("%d\n", m);
+    return (0);
+}
