4a76ff280dc3bc377d6227c755b0ae36ee108718
yemsal4media <yemsalforlove2012@gmail.com>
2023-04-10 15:42:35 -0700

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *  main - a program that prints all arguments it receives
+ *
+ *  @argc: holds the number of argument passed
+ *  @argv: arrays pointer
+ *
+ *  Return: 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	
+	return (0);
+}
