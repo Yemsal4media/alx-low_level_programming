4b57335f29b9bd80f89b9a463b2254bee0b1ca58
yemsal4media <yemsalforlove2012@gmail.com>
2023-04-15 20:20:15 -0700

    progress

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * atoi - this function converts a string
+ *       into an integer
+ *
+ * @s: string input pointer
+ *
+ * Return: 0 if there is no number
+*/
+
+int _atoi(char *s)
+{
+
+}
+
--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,14 +1,13 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - Entry for code check
+ * main - Entry check alx code
  *
- * Return: Always 0 (Success)
- */
+ * Return: Always 0 (success)
+*/
 
 int main(void)
 {
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+	return (0);
 }
